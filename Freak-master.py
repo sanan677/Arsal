bc3df14a79f244b4fcd8c1204ef803073a1766da
sanan677 <74844389+sanan677@users.noreply.github.com>
2020-11-22 12:47:30 +0500

    Add files via upload

---
+++ Freak-master.py
@@ -0,0 +1,11 @@
+<script>
+  import Feather from "feather-helper";
+
+  const githubIcon = Feather("github", {
+    class: "flex",
+    color: "#F09383",
+    "stroke-width": 3,
+  });
+</script>
+
+{@html githubIcon}
